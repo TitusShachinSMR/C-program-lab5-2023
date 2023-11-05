b568138c938d9c15c3024a9f36cbfb423b3a87ca
TitusShachinSMR <143421175+TitusShachinSMR@users.noreply.github.com>
2023-11-04 22:21:05 -0700

    Update array8.cpp

--- array8.cpp
+++ array8.cpp
@@ -1,43 +1,27 @@
+\\deleting duplicate of array
 #include<stdio.h>
 int main()
-{   int i,j,temp1,temp2,l=0;
- int n;
- printf("enter the lenght of the array\n");
- scanf("%d",&n);
- int a[n];
- printf("enter the array:\n");
- for(i=0;i<n;i++)
- {scanf("%d",&a[i]);
- }
- for(i=0;i<n-1;i++)
- {for(j=0;j<n-1-i;j++)
- if(a[j]>a[j+1])
- {
- temp1=a[j];
- a[j]=a[j+1];
- a[j+1]=temp1;}
- }
- for(i=0;i<n;i++)
- {printf("%d ",a[i]);
- }
- printf("\n");
- 
- for(i=0;i<n;i++)
- {if(a[i]==a[i+1])
- {a[i]=0;
- l=l+1;
- }
- }
- for(i=0;i<n-1;i++)
- {for(j=0;j<n-1-i;j++)
- if(a[j]>a[j+1])
- {
- temp2=a[j];
- a[j]=a[j+1];
- a[j+1]=temp2;}
- }
- for(i=l;i<n;i++)
- {printf("%d ",a[i]);
- }
+{ int n,m,i,j,l=0;
+printf("enter the lenght of array 1\n");
+scanf("%d",&n);int a[n];
 
+printf("enter sorted array1:"); 
+for(i=0;i<n;i++)
+{scanf("%d",&a[i]);
 }
+for(i=0;i<n;i++)
+{for(j=i+1;j<n;j++)
+{if(a[i]==a[j]&&a[i]!=-4000)
+{ a[j]=-4000;l=l+1;
+}
+}
+}
+int t=0;int b[n-l];
+for(i=0;i<n;i++)
+{ if(a[i]!=-4000)
+{ b[t]=a[i];t++;
+}
+}
+for(i=0;i<n-l;i++)
+{printf("%d ",b[i]);
+}}
